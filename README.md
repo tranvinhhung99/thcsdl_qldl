eae554fdde9df37bb95b269453a6af730781e12d
xing1999 <xing@cinnamon.is>
2019-05-17 14:11:04 +0700

    INIT COMMIT

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # thcsdl_qldl
 Đề thi thử môn Cơ sở dữ liệu - Winforms
+## Đề bài:
+![Đề](de.jpg)
+## Cấu trúc Database:
+TINHTHANH: (MATINH, TENTINH)
+
+DIADANH: (MADD, TENDD, MATINH)
+
+NGANSACH: (MATINH, NAM, TIEN)
+
+HOATDONG: (STT, MADD, TENHD, CHIPHI)
---
+++ THCSDL_MT/THCSDL_MT/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace THCSDL_MT
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var f = new Form();
+            Application.Run(f);
+        }
+    }
+}
